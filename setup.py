cc8201af2cf6450916811907bf5911f8f19a2ecd
Eric Gradman <eric@gradman.com>
2009-07-18 20:37:49 -0700

    setup and license

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name        = "pystates",
+    version     = "0.1",
+    packages    = find_packages(),
+
+    author      = "Eric Gradman",
+    author_email  = "eric@gradman.com",
+    description   = "pystates is a simple and powerful state machine framework for python",
+    license       = "MIT",
+)
